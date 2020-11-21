e4039c8895ec3c7ca1eb10a1467522df2a4ae00c
oswald01 <49961157+oswald01@users.noreply.github.com>
2020-11-21 10:01:51 -0600

    randomized rgb values + script for helper functions

---
+++ The Vrd Dimension - Unity project/Assets/_CreatedByUs/Scripts/Objects/ObjectColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ObjectColor : MonoBehaviour
+{
+    [SerializeField]
+    private Color manualColor;
+    public Color randomColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (manualColor.a != 0)
+        {
+            randomColor = manualColor;
+        }
+        else if (randomColor.a == 0) // stop it from re-randomizing the color in Play
+        {
+            float r = Random.Range(0, 255);
+            float g = Random.Range(0, 255);
+            float b = Random.Range(0, 255);
+            randomColor = new Color(r, g, b, 1);
+        }
+    }
+}
---
+++ The Vrd Dimension - Unity project/Assets/_CreatedByUs/Scripts/Utility/Helpers.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Helpers : MonoBehaviour
+// any basic multi-use helper functions
+{
+    public static Helpers instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public IEnumerator Timer(Action<bool> assigner, float timer)
+    {
+        yield return new WaitForSeconds(timer);
+        assigner(true);
+    }
+
+    public IEnumerator WaitOneFrame(Action<bool> assigner)
+    {
+        yield return null;
+        assigner(true);
+    }
+}
