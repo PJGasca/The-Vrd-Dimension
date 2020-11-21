fe849f3d70d87aa5297e37b3dd0379802f2159ba
Alex McChesney <alex.mcchesney@gmail.com>
2020-11-21 10:25:15 -0600

    Start entropy tracking.  Add pad support for left controller.

---
+++ The Vrd Dimension - Unity project/Assets/_CreatedByUs/Scripts/Game/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    public class GameManager : MonoBehaviour
+    {
+        private float entropy;
+
+        private float minEntropy;
+
+        private float maxEntropy;
+
+        private float entropyPercentage;
+
+        public static GameManager Instance { get; private set; }
+
+        public void OnEnable()
+        {
+            // Find all the manipulatable objects in the scene
+            GameObject[] objects = GameObject.FindGameObjectsWithTag("Manipulatable");
+
+            // Work out min/max entropies
+
+        }
+    }
+}
+
