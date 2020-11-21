97028139e503e8e290d326f8f2ef0e1182fe8a91
Alex McChesney <alex.mcchesney@gmail.com>
2020-11-21 11:50:27 -0600

    Set box room collision layer and added light.

--- The Vrd Dimension - Unity project/Assets/_CreatedByUs/Scripts/Game/GameManager.cs
+++ The Vrd Dimension - Unity project/Assets/_CreatedByUs/Scripts/Game/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.Objects;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,13 +15,29 @@ namespace Assets.Scripts.Game
 
         public static GameManager Instance { get; private set; }
 
+        [SerializeField]
+        [Tooltip("The number of the smallest size tetras that make up the largest possible.")]
+        private int maxTetraSize;
+
         public void OnEnable()
         {
             // Find all the manipulatable objects in the scene
             GameObject[] objects = GameObject.FindGameObjectsWithTag("Manipulatable");
 
-            
-
+            int maxTetras = 0;
+            foreach(GameObject obj in objects)
+            {
+                // Assume it's a tetra for now
+                ObjectSize size = obj.GetComponent<ObjectSize>();
+                if(size == null)
+                {
+                    Debug.LogWarning("Found a tetra that didn't have a size component on it.  That can't be right!");
+                }
+                else
+                {
+                    maxTetras += size.Size;
+                }
+            }
         }
     }
 }
