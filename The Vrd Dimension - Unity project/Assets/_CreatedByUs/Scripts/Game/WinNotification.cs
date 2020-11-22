ced0c076d0bf011c2f568f814fecf4776e2b94c3
Erto Pena <ertopena@gmail.com>
2020-11-22 17:50:50 -0600

    Show game win notification text.

---
+++ The Vrd Dimension - Unity project/Assets/_CreatedByUs/Scripts/Game/WinNotification.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Game {
+    public class WinNotification : MonoBehaviour
+    {
+        public MeshRenderer meshRenderer;
+
+
+        void OnEnable () {
+            meshRenderer.enabled = false;
+            EndChecker.OnGameWin += Show;
+        }
+
+
+        void OnDisable () {
+            EndChecker.OnGameWin -= Show;
+        }
+
+
+        void Show () {
+            meshRenderer.enabled = true;
+        }
+    }
+}
