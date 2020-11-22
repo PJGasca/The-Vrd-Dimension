5afc8e8241f80d479ed75caed9ad7248d17c7b09
Alex McChesney <alex.mcchesney@gmail.com>
2020-11-22 15:37:57 -0600

    Add missing scripts

---
+++ The Vrd Dimension - Unity project/Assets/_CreatedByUs/Scripts/Objects/MergableObject.cs
@@ -0,0 +1,238 @@
+using Assets.Scripts.Utility;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Objects {
+    [RequireComponent (typeof (Grabbable), typeof (ObjectSize))]
+    public class MergableObject : MonoBehaviour
+    {
+        public enum ShapeType { Tetrahedron, Octohedron }
+
+#region Constants and static members
+
+        public const float baseSplitVelocity = 0.5f;
+        public const float splitRadius = 0.01f;
+
+
+        private const float _mergeCooldown = 2f;
+
+
+        public static MergableObject[] All => _all.ToArray ();
+        public static Dictionary<int, MergableObject[]> AllBySize {
+            get {
+                Dictionary<int, MergableObject[]> allBySize = new Dictionary<int, MergableObject[]> ();
+                foreach (int key in _allBySize.Keys) {
+                    if (_allBySize[key].Count > 0) { allBySize.Add (key, _allBySize[key].ToArray ()); }
+                }
+                return allBySize;
+            }
+        }
+        public static int[] Sizes => AllBySize.Where (kvp => kvp.Value.Length > 0).Select (kvp => kvp.Key).ToArray ();
+
+
+        private static List<MergableObject> _all = new List<MergableObject> ();
+        private static Dictionary<int, List<MergableObject>> _allBySize = new Dictionary<int, List<MergableObject>> ();
+
+
+        public static MergableObject[] AllForSize (int size) {
+            if (!_allBySize.ContainsKey (size) || _allBySize[size].Count == 0) { return new MergableObject[0]; }
+            return _allBySize[size].ToArray ();
+        }
+#endregion
+
+
+#region Instance members
+
+        public event System.Action<MergableObject> OnShapeEnabled;
+        public event System.Action<MergableObject> OnShapeSplit;
+
+
+        public Vector3 SpawnPosition { get; private set; }
+        public bool CanMerge => _HasMovedFromScenePosition && !_OnMergeCooldown;
+
+
+        private bool _OnMergeCooldown { get; set; }
+        private bool _HasMovedFromScenePosition { get; set; }
+
+
+        public bool targetedByAgent;
+
+        [SerializeField]
+        private ShapeType shapeType = ShapeType.Tetrahedron;
+
+
+        [SerializeField] private ObjectSize _sizeComponent;
+        private Coroutine _mergeTimerCoroutine;
+        private bool _didWatchPositionCheck;
+
+
+        void Awake () {
+            if (_sizeComponent == null) { _sizeComponent = GetComponent<ObjectSize> (); }
+        }
+
+
+        void OnEnable () {
+            SpawnPosition = transform.position;
+            _all.Add (this);
+            Game.GameManager.Instance?.OnObjectAdded (this.gameObject);
+            InitializeSize ();
+
+            if (OnShapeEnabled != null) { OnShapeEnabled (this); }
+        }
+
+
+        void OnDisable () {
+            targetedByAgent = false;
+            if (_all.Contains (this)) { _all.Remove (this); }
+            if (_allBySize.ContainsKey (_sizeComponent.Size) && _allBySize[_sizeComponent.Size].Contains (this)) {
+                _allBySize[_sizeComponent.Size].Remove (this);
+            }
+            Game.GameManager.Instance?.OnObjectRemoved (this.gameObject);
+
+            if (_didWatchPositionCheck) { _HasMovedFromScenePosition = true; }
+            else { Debug.LogWarning ("Shape called OnDisable before calling Start()!"); }
+        }
+
+
+        void Start () {
+            StartCoroutine (WatchPositionInScene ());
+        }
+
+
+        public void Split () {
+            if (_sizeComponent.Size < MergableObjectManager.Instance.countForMerge) { return; }
+
+            GameObject particles = ObjectPool.Instance.GetObjectForType("SplitParticles");
+            particles.transform.position = transform.position;
+
+            MergableObject[] newShapes = ShapesForSplit ();
+
+            Vector3 center = transform.position;
+            int[] sizes = SplitSizes ();
+
+            Vector3[] vertices = VertexOffsets (center);
+
+            for (int i = 0; i < newShapes.Length; i++) {
+                newShapes[i].transform.position = PositionForSplit (vertices[i], center);
+                // newTetrahedra[i].GetComponent<Rigidbody> ().velocity = VelocityForSplit (vertices[i]);
+                newShapes[i].SetSize (sizes[i], _sizeComponent.Size);
+                newShapes[i].gameObject.SetActive (true);
+            }
+
+            if (OnShapeSplit != null) { OnShapeSplit (this); }
+        }
+
+
+        public void SetSize (int newSize) {
+            SetSize (newSize, _sizeComponent.Size);
+        }
+        public void SetSize (int newSize, int oldSize) {
+            StartMergeCooldownTimer ();
+            _sizeComponent.Size = newSize;
+
+            SetScale (newSize);
+            RegisterBySize (newSize, oldSize);
+        }
+
+
+        void InitializeSize () {
+            SetSize (_sizeComponent.Size, -1);
+        }
+
+
+        void StartMergeCooldownTimer () {
+            if (_mergeTimerCoroutine != null) { return; }
+
+            _OnMergeCooldown = true;
+            _mergeTimerCoroutine = StartCoroutine (RunMergeCooldownTimer ());            
+        }
+
+
+        IEnumerator RunMergeCooldownTimer () {
+            yield return new WaitForSeconds (_mergeCooldown);
+            _OnMergeCooldown = false;
+            _mergeTimerCoroutine = null;
+        }
+
+
+        void SetScale (int newSize) {
+            MergableObjectManager manager = MergableObjectManager.Instance;
+            //transform.localScale = manager.minimumScale * (1 + Mathf.Log (newSize, manager.countForMerge)) * Vector3.one;
+
+            transform.localScale = (manager.minimumScale * newSize) * Vector3.one;
+        }
+
+
+        MergableObject[] ShapesForSplit () {
+            MergableObject[] newShapes = new MergableObject[MergableObjectManager.Instance.countForMerge];
+            newShapes[0] = this;
+            for (int i = 1; i < MergableObjectManager.Instance.countForMerge; i++) {
+                newShapes[i] = GetShapeFromPool ();
+            }
+
+            return newShapes;
+        }
+
+
+        int[] SplitSizes () {
+            int[] sizes = new int[MergableObjectManager.Instance.countForMerge];
+
+            int average = _sizeComponent.Size / MergableObjectManager.Instance.countForMerge;
+            int remainder = _sizeComponent.Size % MergableObjectManager.Instance.countForMerge;
+
+            sizes[0] = average + remainder;
+            for (int i = 1; i < sizes.Length; i++) { sizes[i] = average; }
+
+            return sizes;
+        }
+
+
+        Vector3[] VertexOffsets (Vector3 center) {
+            return GetComponent<MeshFilter> ().mesh.vertices
+                .Select (v => transform.TransformPoint (v))
+                .Select (v => (v - center).normalized)
+                .ToArray ();
+        }
+
+
+        Vector3 PositionForSplit (Vector3 normalizedOffset, Vector3 center) {
+            return center + normalizedOffset * splitRadius;
+        }
+
+
+        // Vector3 VelocityForSplit (Vector3 normalizedOffset) {
+        //     return normalizedOffset * baseSplitVelocity / _sizeComponent.Size;
+        // }
+
+
+        void RegisterBySize (int newSize, int oldSize) {
+            if (!_allBySize.ContainsKey (newSize)) { _allBySize.Add (newSize, new List<MergableObject> ()); }
+            _allBySize[newSize].Add (this);
+
+            if (!_allBySize.ContainsKey (oldSize)) { return; }
+            if (_allBySize[oldSize].Contains (this)) { _allBySize[oldSize].Remove (this); }
+        }
+
+
+        IEnumerator WatchPositionInScene () {
+            _didWatchPositionCheck = true;
+            Vector3 positionInScene = transform.position;
+            do {
+                yield return new WaitForEndOfFrame ();
+            } while (Vector3.Distance (transform.position, positionInScene) < 0.05f);
+            
+            _HasMovedFromScenePosition = true;
+        }
+
+
+        // TODO: ENABLE POOLING
+        MergableObject GetShapeFromPool () {
+            return Utility.ObjectPool.Instance.GetObjectForType (shapeType.ToString(), true).GetComponent<MergableObject> ();
+            //return Instantiate (this);
+        }
+
+#endregion
+    }
+}
---
+++ The Vrd Dimension - Unity project/Assets/_CreatedByUs/Scripts/Objects/MergableObjectManager.cs
@@ -0,0 +1,114 @@
+﻿using Assets.Scripts.Utility;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Objects {
+    public class MergableObjectManager : MonoBehaviour
+    {
+
+        public static MergableObjectManager Instance {
+            get {
+                if (_Instance == null) { _Instance = FindObjectOfType<MergableObjectManager> (); }
+                return _Instance;
+            }
+        }
+        private static MergableObjectManager _Instance { get; set; }
+
+
+        public float baseMergeRadius = 0.5f;
+        public int countForMerge = 2;
+        public float minimumScale = 30f;    
+        public bool mergeOnlyEqualSizes = false;
+
+        public void OnEnable()
+        {
+            StartCoroutine(IntermittentCheckForMerges());
+        }
+
+        System.Collections.IEnumerator IntermittentCheckForMerges() {
+            WaitForSeconds wait = new WaitForSeconds(0.1f);
+            while(true)
+            {
+                if (mergeOnlyEqualSizes)
+                    CheckForMergesBySize();
+                else
+                    CheckForMerges(MergableObject.All);
+
+                yield return wait;
+            }
+        }
+
+
+        void CheckForMergesBySize () {
+            var allBySize = MergableObject.AllBySize;
+            int[] sizes = MergableObject.Sizes;
+
+            foreach (int size in sizes) {
+                MergableObject[] all = allBySize[size];
+                CheckForMerges (all);
+            }
+        }
+
+
+        void CheckForMerges (MergableObject[] all) {
+           // Debug.Log("Checking for merges");
+            all = all.Where (t => t.CanMerge).ToArray ();
+            if (all.Length < countForMerge) { return; }
+
+            List<MergableObject> unused = all.ToList ();
+            foreach (MergableObject t in all) {
+                if (!unused.Contains (t)) { continue; }
+
+                float mergeRadius = baseMergeRadius * t.transform.localScale.x / minimumScale;
+                var inRange = unused.Where (u => Vector3.Distance (u.transform.position, t.transform.position) < mergeRadius).ToArray ();
+                if (inRange.Length < countForMerge) { continue; }
+
+                MergableObject[] merging = inRange.Take (countForMerge).ToArray ();
+                Merge (merging);
+                foreach (var merged in merging) { unused.Remove (merged); }
+
+                //Debug.Log ($"{unused.Count} tetrahedra remaining in merging group");
+            }
+        }
+
+
+        void Merge (MergableObject[] toMerge) {
+            string debugSizes = toMerge.Select(t => t.GetComponent<ObjectSize> ().Size.ToString ()).Aggregate (string.Empty, (s, acc) => s + acc + ", ");
+            Debug.Log ($"Merging sizes: {debugSizes}");
+
+            int mergedSize = MergedSize (toMerge);
+            toMerge[0].SetSize (mergedSize);
+
+            //Vector3 mergedPosition = MergedTetrahedronPosition (toMerge, mergedSize);
+            //Debug.Log("Merged pos is " + mergedPosition + " source pos is " + toMerge[0].transform.position);
+           // toMerge[0].transform.position = mergedPosition;
+
+            GameObject mergeParticles = ObjectPool.Instance.GetObjectForType("TriangularImplosion");
+            mergeParticles.transform.position = toMerge[0].transform.position;
+
+            for (int i = 1; i < toMerge.Length; i++) {
+                PoolShape (toMerge[i]);
+            }
+        }
+
+
+      /*  Vector3 MergedTetrahedronPosition (MergableObject[] tetrahedra, int mergedSize) {
+            return tetrahedra
+                .Select (t => t.transform.position * t.GetComponent<ObjectSize> ().Size)
+                .Aggregate (Vector3.zero, (p, acc) => acc + p) / mergedSize;
+        }*/
+
+
+        int MergedSize (MergableObject[] tetrahedra) {
+            return tetrahedra
+                .Select (t => t.GetComponent<ObjectSize> ().Size)
+                .Aggregate (0, (size, acc) => size + acc);
+        }
+
+        void PoolShape (MergableObject tetrahedron) {
+            Utility.ObjectPool.Instance.PoolObject (tetrahedron.gameObject);
+            //Destroy (tetrahedron.gameObject);
+        }
+    }
+}
