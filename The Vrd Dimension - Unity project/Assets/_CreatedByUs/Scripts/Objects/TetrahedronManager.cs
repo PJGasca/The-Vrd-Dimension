c24cc721f891f445eab250bbee302d3f2df02b60
Erto Pena <ertopena@gmail.com>
2020-11-21 23:03:10 -0600

    Open up tetrahedron merging parameters (size and count).

--- The Vrd Dimension - Unity project/Assets/_CreatedByUs/Scripts/Objects/TetrahedronManager.cs
+++ The Vrd Dimension - Unity project/Assets/_CreatedByUs/Scripts/Objects/TetrahedronManager.cs
@@ -9,23 +9,28 @@ namespace Assets.Scripts.Objects {
         public const float mergeRadius = 0.5f;
 
 
+        public bool mergeOnlyEqualSizes = false;
+
+
         void FixedUpdate () {
-            CheckForMerges ();
+            if (mergeOnlyEqualSizes)
+                CheckForMergesBySize ();
+            else
+                CheckForMerges (Tetrahedron.All);
         }
 
 
-        void CheckForMerges () {
+        void CheckForMergesBySize () {
             var allBySize = Tetrahedron.AllBySize;
             int[] sizes = Tetrahedron.Sizes;
-            Debug.Log ("Size count for merge: " + sizes.Length);
+
             foreach (int size in sizes) {
                 Tetrahedron[] all = allBySize[size];
-                Debug.Log ($"{all.Length} tetrahedra of size {size}");
                 CheckForMerges (all);
             }
-
-            Debug.Log ($"Remaining tetrahedra: {Tetrahedron.All.Length}");
         }
+
+
         void CheckForMerges (Tetrahedron[] all) {
             if (all.Length < countForMerge) { return; }
 
@@ -36,20 +41,23 @@ namespace Assets.Scripts.Objects {
                 var inRange = unused.Where (u => Vector3.Distance (u.transform.position, t.transform.position) < mergeRadius).ToArray ();
                 if (inRange.Length < countForMerge) { continue; }
 
-                Debug.Log ("Merging!");
-
                 Tetrahedron[] merging = inRange.Take (countForMerge).ToArray ();
                 Merge (merging);
-                // unused.RemoveAll (u => merging.Contains (u));
                 foreach (var merged in merging) { unused.Remove (merged); }
+
+                Debug.Log ($"{unused.Count} tetrahedra remaining in merging group");
             }
         }
 
 
-        void Merge (Tetrahedron[] tetrahedra) {            
-            int newSize = tetrahedra[0].GetComponent<ObjectSize> ().Size * countForMerge;
-            tetrahedra[0].SetSize (newSize);
-            Vector3 mergedPosition = MergedTetrahedronPosition (tetrahedra);
+        void Merge (Tetrahedron[] tetrahedra) {
+            string debugSizes = tetrahedra.Select(t => t.GetComponent<ObjectSize> ().Size.ToString ()).Aggregate (string.Empty, (s, acc) => s + acc + ", ");
+            Debug.Log ($"Merging sizes: {debugSizes}");
+
+            int mergedSize = MergedTetrahedronSize (tetrahedra);
+            tetrahedra[0].SetSize (mergedSize);
+
+            Vector3 mergedPosition = MergedTetrahedronPosition (tetrahedra, mergedSize);
             tetrahedra[0].transform.position = mergedPosition;
             // TODO: Instantiate particle prefab at mergedPosition
 
@@ -59,10 +67,17 @@ namespace Assets.Scripts.Objects {
         }
 
 
-        Vector3 MergedTetrahedronPosition (Tetrahedron[] tetrahedra) {
+        Vector3 MergedTetrahedronPosition (Tetrahedron[] tetrahedra, int mergedSize) {
             return tetrahedra
-                .Select (t => t.transform.position)
-                .Aggregate (Vector3.zero, (p, acc) => acc + p) / countForMerge;
+                .Select (t => t.transform.position * t.GetComponent<ObjectSize> ().Size)
+                .Aggregate (Vector3.zero, (p, acc) => acc + p) / mergedSize;
+        }
+
+
+        int MergedTetrahedronSize (Tetrahedron[] tetrahedra) {
+            return tetrahedra
+                .Select (t => t.GetComponent<ObjectSize> ().Size)
+                .Aggregate (0, (size, acc) => size + acc);
         }
 
 
